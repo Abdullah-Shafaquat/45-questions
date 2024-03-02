bcb85c4fdc62523cff98647a7bf722a50d34133f
M.Abdullah <159773514+Abdullah-Shafaquat@users.noreply.github.com>
2024-03-02 23:46:52 +0500

    Add files via upload
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Q12.js
@@ -0,0 +1,7 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var name1 = ['Hello Abdullah!', 'Hello Abubakar!', 'Hello Areesh!', 'Hello Tayyab!'];
+var messege = "How's your day going?";
+console.log(name1[0], messege);
+console.log(name1[1], messege);
+console.log(name1[2], messege);
+console.log(name1[3], messege);
---
+++ Q12.ts
@@ -0,0 +1,8 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let name1 = ['Hello Abdullah!', 'Hello Abubakar!' , 'Hello Areesh!' , 'Hello Tayyab!'];
+let messege = "How's your day going?"
+console.log(name1[0] , messege);
+console.log(name1[1] , messege);
+console.log(name1[2] , messege);
+console.log(name1[3] , messege);
