14202ead76da29f03441036e98d74cd8f73f7727
M.Abdullah <159773514+Abdullah-Shafaquat@users.noreply.github.com>
2024-03-02 22:08:49 +0500

    Add files via upload
    Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ Q9.ts
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+let favoritenumber = 4;
+let messege ="My lucky number is"
+console.log(messege, favoritenumber);
